29fc90da6197d544424816d40b1f1f4ce8c4f3af
taixingbi <bitaihang@gmail.com>
2020-03-12 12:02:51 -0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,16 +1,4 @@
 
-# #broker='sqs://AKIARJLDNDHBUNFR7PVK:lWpWXK91cRaKkFJwoCzO6tE24XhfdQEsfT8Gzj57@'
-# redis='redis://localhost:6379'
-# app = Celery('tasks', backend=redis, broker=redis)
-# app.config_from_object('celeryconfig')
-
-# from __future__ import absolute_import, unicode_literals
-#from celery import app
-
-#from celery import Celery
-
-#app = Celery('proj', broker=redis, backend=redis)
-#app.config_from_object('celeryconfig')
 
 from __future__ import absolute_import, unicode_literals
 
